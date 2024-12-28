3bb36b2b192a8f270432636fc3efa7511066d87a
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-12-29 00:59:55 +0530

    express file upload

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ dotenv.config()
 import express from 'express';
 import cors from 'cors'
 import morgan from 'morgan';
+import fileUpload from 'express-fileupload'
+import userRouter from './routes/user.route.js';
 
 
 const app = express();
@@ -11,12 +13,19 @@ const app = express();
 // express middlewares
 app.use(cors());
 app.use(morgan('dev'))
+app.use(fileUpload({
+    useTempFiles: true,
+    // tempFileDir : '/tmp/'
+    // limits : {fileSize : 50 * 1024 * 1024}
+}))
 app.use(express.json());
 
 app.get('*', (req, res) => {
     res.send('Welcome');
 })
 
+// routes
+app.use('/api/user' , userRouter)
 
 export { app }
 
