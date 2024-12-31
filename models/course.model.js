0f19bae469a2bf327ec0601ff76a0298fab4fd48
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-12-31 23:51:26 +0530

    course model

---
+++ models/course.model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const courseSchema = new mongoose.Schema({
+    courseName: {
+        type: String,
+        required: [true, 'Course name is required'],
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: [true, 'Course price is required'],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+        trim: true,
+    },
+    startDate: {
+        type: String,
+        required: [true, 'Start date is required'],
+    },
+    endDate: {
+        type: String,
+        required: [true, 'End date is required'],
+    },
+    image: {
+        public_id: {
+            type: String,
+            required: [true, "Image ID is required"],
+        },
+        url: {
+            type: String,
+            required: [true, "Image URL is required"],
+        },
+    },
+    courseAdmin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+// Exporting the User model
+const CourseModel = mongoose.model('User', courseSchema);
+
+export default CourseModel;
